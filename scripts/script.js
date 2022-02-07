9259d21169efe1e1c4036262c4cbd3ef6d804a09
Parth-26 <parthbpatel26102000@gmail.com>
2022-02-07 15:48:49 +0530

    Final Commit

---
+++ scripts/script.js
@@ -0,0 +1,86 @@
+// Handle errors.
+let handleError = function(err){
+    console.log("Error: ", err);
+};
+
+// Query the container to which the remote stream belong.
+let remoteContainer = document.getElementById("remote-container");
+
+// Add video streams to the container.
+function addVideoStream(elementId){
+    // Creates a new div for every stream
+    let streamDiv = document.createElement("div");
+    // Assigns the elementId to the div.
+    streamDiv.id = elementId;
+    // Takes care of the lateral inversion
+    streamDiv.style.transform = "rotateY(180deg)";
+    // Adds the div to the container.
+    remoteContainer.appendChild(streamDiv);
+};
+
+// Remove the video stream from the container.
+function removeVideoStream(elementId) {
+    let remoteDiv = document.getElementById(elementId);
+    if (remoteDiv) remoteDiv.parentNode.removeChild(remoteDiv);
+};
+let client = AgoraRTC.createClient({
+    mode: "rtc",
+    codec: "vp8",
+});
+
+client.init("82d6463100bc4ce0a04d1be5d953c90b", function() {
+    console.log("client initialized");
+}, function(err) {
+    console.log("client init failed ", err);
+});
+// Join a channel
+client.join("00682d6463100bc4ce0a04d1be5d953c90bIAAlLoB5Qw6vs7zVt0XwLhEjBcziyhyS3YeV2T6+3AzkF7G17oYAAAAAEAD1z9KPPkECYgEAAQA7QQJi", "videoapp", null, (uid)=>{
+    // Create a local stream
+  }, handleError);
+  let localStream = AgoraRTC.createStream({
+    audio: true,
+    video: true,
+});
+// Initialize the local stream
+localStream.init(()=>{
+    // Play the local stream
+    localStream.play("me");
+    // Publish the local stream
+    client.publish(localStream, handleError);
+}, handleError);
+// Subscribe to the remote stream when it is published
+client.on("stream-added", function(evt){
+    client.subscribe(evt.stream, handleError);
+});
+// Play the remote stream when it is subsribed
+client.on("stream-subscribed", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    addVideoStream(streamId);
+    stream.play(streamId);
+});
+// Subscribe to the remote stream when it is published
+client.on("stream-added", function(evt){
+    client.subscribe(evt.stream, handleError);
+});
+// Play the remote stream when it is subsribed
+client.on("stream-subscribed", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    addVideoStream(streamId);
+    stream.play(streamId);
+});
+// Remove the corresponding view when a remote user unpublishes.
+client.on("stream-removed", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close();
+    removeVideoStream(streamId);
+});
+// Remove the corresponding view when a remote user leaves the channel.
+client.on("peer-leave", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close();
+    removeVideoStream(streamId);
+});
